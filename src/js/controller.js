72e55524db280ced6b95e460e85965e930f313be
ShaikArsalan347 <shaikarsalan347@gmail.com>
2025-01-24 22:52:52 +0530

    create model and add functionlty like :#getSearchResultsPag,#loadSearchResult

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,30 +1,51 @@
 import "core-js/stable";
 import "regenerator-runtime/runtime";
 //
-import * as model from "./model.js";
 import recipeView from "./views/recipeView.js";
+import searchView from "./views/searchView.js";
+import resultView from "./views/resultView.js";
+import * as model from "./model.js";
+///////////////////////////////////////\
+if (module.hot) {
+  module.hot.accept();
+}
 
-// const recipeContainer = document.querySelector(".recipe");
-
-// https://forkify-api..com/v2
-
-///////////////////////////////////////
 console.log(model.state);
 const controlRecipes = async function () {
-  const id = window.location.hash.slice(1);
-  if (!id) return;
-  recipeView.renderSpinner();
-  // load recipe
-  await model.loadRecipe(id);
-  //rendering recipe
-  recipeView.render(model.state.recipe);
   try {
+    const id = window.location.hash.slice(1);
+    if (!id) return;
+    recipeView.renderSpinner();
+    // load recipe
+    await model.loadRecipe(id);
+    //rendering recipe
+    recipeView.render(model.state.recipe);
   } catch (err) {
-    alert(err);
+    console.error(err);
+    recipeView.renderError();
   }
-  // controlRecipes();
 };
+
+const controlSearchResult = async function () {
+  try {
+    resultView.renderSpinner();
+
+    // get search query
+    const query = searchView.getQuery();
+    if (!query) return;
+    // load search result
+    await model.loadSearchResult(query);
+    // reander result
+    console.log(model.state.search.results);
+    resultView.render(model.getSearchResultsPage());
+  } catch (err) {
+    console.log(err);
+  }
+};
+// controlSearchResult();
+
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
+  searchView.addHandlerSearch(controlSearchResult);
 };
 init();
--- src/js/model.js
+++ src/js/model.js
@@ -1,13 +1,20 @@
 import { async } from "regenerator-runtime";
-import { API_URL } from "./views/config.js";
+import { API_URL, RES_PER_PAGE } from "./views/config.js";
 import { getJSON } from "./helpers.js";
+// import { search } from "core-js/fn/symbol";
 export const state = {
   recipe: {},
+  search: {
+    query: "",
+    results: [],
+    resultsPerPage: RES_PER_PAGE,
+    page: 1,
+  },
 };
 
 export const loadRecipe = async function (id) {
   try {
-    const data = await getJSON(`${API_URL}/${id}`);
+    const data = await getJSON(`${API_URL}${id}`);
     // Transform the recipe data and store it in state
     const { recipe } = data.data;
     state.recipe = {
@@ -22,5 +29,30 @@ export const loadRecipe = async function (id) {
     };
   } catch (err) {
     console.error(`${err} 🔴🔴`);
+    throw err;
   }
+};
+export const loadSearchResult = async function (query) {
+  state.search.query = query;
+  const data = await getJSON(`${API_URL}?search=${query}`);
+  console.log(data);
+  try {
+    state.search.results = data.data.recipes.map((rec) => {
+      return {
+        id: rec.id,
+        title: rec.title,
+        publisher: rec.publisher,
+        image: rec.image_url,
+      };
+    });
+  } catch (err) {
+    console.error(`${err} 🔴🔴`);
+    throw err;
+  }
+};
+export const getSearchResultsPage = function (page = state.search.page) {
+  state.search.page;
+  const start = (page - 1) * state.search.resultsPerPage;
+  const end = page * state.search.resultsPerPage;
+  return state.search.results.slice(start, end);
 };

b869b09e93be308eb5be6491c7710203b25d2d10
ShaikArsalan347 <shaikarsalan347@gmail.com>
2025-01-30 19:20:20 +0530

    pagination cmp

--- src/js/controller.js
+++ src/js/controller.js
@@ -4,10 +4,17 @@ import "regenerator-runtime/runtime";
 import recipeView from "./views/recipeView.js";
 import searchView from "./views/searchView.js";
 import resultView from "./views/resultView.js";
+import paginationView from "./views/paginationView.js";
 import * as model from "./model.js";
 ///////////////////////////////////////\
+// if (module.hot) {
+//   module.hot.accept();
+// }
 if (module.hot) {
+  console.log("✅ HMR is enabled!");
   module.hot.accept();
+} else {
+  console.log("❌ HMR is NOT working!");
 }
 
 console.log(model.state);
@@ -27,25 +34,39 @@ const controlRecipes = async function () {
 };
 
 const controlSearchResult = async function () {
+  // Default to page 1
   try {
     resultView.renderSpinner();
 
-    // get search query
+    // Get search query
     const query = searchView.getQuery();
     if (!query) return;
-    // load search result
+
+    // Load search result
     await model.loadSearchResult(query);
-    // reander result
-    console.log(model.state.search.results);
+
+    // Render result
     resultView.render(model.getSearchResultsPage());
+    // resultView.render(resultsPage);
+
+    // Render pagination buttons
+    paginationView.render(model.state.search);
   } catch (err) {
     console.log(err);
   }
 };
-// controlSearchResult();
 
+// controlSearchResult();
+const controlpagination = function (goToPage) {
+  // Render new result
+  resultView.render(model.getSearchResultsPage(goToPage));
+
+  // Render new pagination buttons
+  paginationView.render(model.state.search);
+};
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
   searchView.addHandlerSearch(controlSearchResult);
+  paginationView._addHandlerClick(controlpagination);
 };
 init();
